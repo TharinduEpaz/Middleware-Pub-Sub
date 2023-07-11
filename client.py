38933ec1c58b2602ef024bbb211846fe30616b01
Tharindu Epasingha <2020cs050@stu.ucsc.cmb.ac.lk>
2023-07-11 16:05:57 +0530

    added command line argument support

--- client.py
+++ client.py
@@ -1,11 +1,24 @@
 import socket
+import sys
 
 PORT = 5050
 HEADER = 64
-SERVER = '127.0.0.1'
+SERVER = '192.168.8.205'
 ADDR = (SERVER, PORT)
 FORMAT = "utf-8"
 DISCONNECT_MSG = "!DISCONNECT"
 
+# Extract the command-line arguments
+
+# script_name = sys.argv[0]
+# ip_address = sys.argv[1]
+# port = sys.argv[2]
+# role = sys.argv[3]
+message = sys.argv[1]
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+
+
+client.send(message.encode(FORMAT))
